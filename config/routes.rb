16dba0473638a2d470b41978f0dd65d57158d057
Saikrishna Rao <krish98.sai@gmail.com>
2017-09-27 11:39:45 -0700

    Added devise, but didn't migrate db

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'home/new'
 
   get 'home/create'
+
+  root to: "home#new"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
